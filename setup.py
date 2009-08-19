efd34a93e75b15ae03b3acacd75b352a8be7eb5a
Alexander Kessinger <voidfiles@gmail.com>
2009-08-18 22:21:17 -0700

    adding the modile, and a setup.py lets see if it works

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name         = 'id3reader',
+      description  = 'Id3reader.py is a Python module that reads ID3 metadata tags in MP3 files.',
+      author       = 'Ned Batchelder',
+      author_email = 'voidfiles@gmail.com',
+      url          = 'http://github.com/voidfiles/id3reader/tree/master',
+      version      = '1.53.20070415',
+      py_modules   = ['id3reader'],
+     )
